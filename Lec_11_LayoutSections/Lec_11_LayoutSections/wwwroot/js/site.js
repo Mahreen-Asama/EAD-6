b64306ac943fbae5880bf175cb3fb77320e85f69
Mahreen-Asama <mahreenmehar202@gmail.com>
2022-05-16 20:21:12 +0500

    js section added

--- Lec_11_LayoutSections/Lec_11_LayoutSections/wwwroot/js/site.js
+++ Lec_11_LayoutSections/Lec_11_LayoutSections/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+document.getElementById("s1h1").innerHTML = fun1();
+
+var v = 0;
+function fun1() {
+    return "Hello world";
+}
