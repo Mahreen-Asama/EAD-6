fedbcdf6dbdb3654520a67335b8a69fc8be488ea
Mahreen-Asama <mahreenmehar202@gmail.com>
2022-05-16 21:54:45 +0500

    middlewares added

--- Lec_11_MiddleWares/Lec_11_MiddleWares/Program.cs
+++ Lec_11_MiddleWares/Lec_11_MiddleWares/Program.cs
@@ -1,17 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.Use(async (context, next) => {
-    context.Response.ContentType = "text/plain";
-    await context.Response.WriteAsync("Aoa Pak...");
-    await next();
-});
-
-app.Use(async (context, next) => {
-    await context.Response.WriteAsync("Welcome to class");
-    await next();
-});
 
 app.MapGet("/", () => "Hello World!");
 
+app.Use(async (context, next) => {
+    context.Response.ContentType = "text/plain";
+    await context.Response.WriteAsync("Welcome to class");
+    await next();
+});
+app.Use(async (context, next) => {
+    await context.Response.WriteAsync("Aoa Pak...");
+    await next();
+});
 app.Run();
