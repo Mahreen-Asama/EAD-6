e6d34b6468bfe0dfe92ec0c5abbdcbe944bb19ae
Mahreen-Asama <mahreenmehar202@gmail.com>
2022-05-30 23:58:06 +0500

    one vc running

---
+++ Lec11,12_LayoutSections,PartialViews/Lec_11_LayoutSections/ViewComponents/TaxSummary.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lec_11_LayoutSections.ViewComponents
+{
+    public class TaxSummary:ViewComponent
+    {
+        public IViewComponentResult Invoke(int taxRate, int taxAmount)
+        {
+            object data = $"tax rate {taxRate} adn {taxAmount}";
+            return View(data);
+        }
+    }
+}
---
+++ Lec11,12_LayoutSections,PartialViews/Lec_11_LayoutSections/ViewComponents/WeatherSummary.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lec_11_LayoutSections.ViewComponents
+{
+    public class WeatherSummary:ViewComponent
+    {
+        public string Invoke()
+        {
+            string data = "this is weather informatoin";
+            return data;
+        }
+    }
+}
