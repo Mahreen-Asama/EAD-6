81b5f65e80d4aac9d30476680169eddf33314b6f
Mahreen-Asama <mahreenmehar202@gmail.com>
2022-07-14 13:02:50 +0500

    migration done

---
+++ Asp.Net Core Identity/Asp.Net Core Identity/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.Net_Core_Identity.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Asp.Net Core Identity/Asp.Net Core Identity/Program.cs
+++ Asp.Net Core Identity/Asp.Net Core Identity/Program.cs
@@ -1,4 +1,23 @@
+using Asp.Net_Core_Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Getting Connection string
+string connString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+//Getting Assembly Name
+var migrationAssembly = typeof(Program).Assembly.GetName().Name;
+
+// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+           options.UseSqlServer(connString, sql => sql.MigrationsAssembly(migrationAssembly)));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>();
+
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
