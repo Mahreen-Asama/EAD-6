4dda269a7edb1b77e19696838115738ec19f63c6
Mahreen-Asama <mahreenmehar202@gmail.com>
2022-07-14 13:59:21 +0500

    sign up ok

--- Asp.Net Core Identity/Asp.Net Core Identity/Controllers/AccountController.cs
+++ Asp.Net Core Identity/Asp.Net Core Identity/Controllers/AccountController.cs
@@ -1,18 +1,65 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Asp.Net_Core_Identity.ViewModels;
 
 namespace Asp.Net_Core_Identity.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> uManager,
+                                 SignInManager<IdentityUser> sManager)
         {
-            return View();
+            userManager = uManager;
+            signInManager = sManager;
         }
 
+        //-------------signup-------------
         [HttpGet]
         public IActionResult Register()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser
+                {
+                    UserName = model.Username,
+                    Email = model.Email,
+                    EmailConfirmed = true,
+                    LockoutEnabled = false,
+                };
+                var result = await userManager.CreateAsync(user,
+                                                         model.Password);
+
+                if (result.Succeeded)
+                {
+                    if (signInManager.IsSignedIn(User))
+                    {
+                        return RedirectToAction("index", "home");
+                    }
+                    return RedirectToAction("login", "account");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View();
+        }
+
+        //------login-----------
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
     }
 }
