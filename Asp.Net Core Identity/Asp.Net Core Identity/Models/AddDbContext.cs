10717ca71d4404ad6ab1efd73a93a0d66b5e3ecf
Mahreen-Asama <mahreenmehar202@gmail.com>
2022-07-14 07:09:13 +0500

    packages installed

---
+++ Asp.Net Core Identity/Asp.Net Core Identity/Models/AddDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Asp.Net_Core_Identity.Models
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
